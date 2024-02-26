35b4f076710bc1ee643fb4d26d5f29f736bf45c1
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-02-26 11:00:48 +0900

    파이썬 시작

---
+++ example1.py
@@ -0,0 +1,6 @@
+eric="Hello, Eric. it is a good day to learn Python"
+print(eric)
+name='junekim'
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
---
+++ hello_world.py
@@ -0,0 +1,16 @@
+message = "Hello Python World!"
+print(message)
+print("Hello")
+
+name="ada lovelace"
+print(name.title())
+
+first_name="ada"
+last_name="lovelace"
+full_name=f"{first_name}\n {last_name.title()}!"
+print(full_name)
+print('language: \npython \tjava')
+
+favoritelanguage = 'python '
+print(favoritelanguage)
+print(favoritelanguage.rstrip())
