b2ae402dd87df479ce2e1bb2ef6e62502509e860
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-02-29 17:50:07 +0900

    임포트방법 하나 더

--- chapter08/making_pizza.py
+++ chapter08/making_pizza.py
@@ -4,3 +4,6 @@ pizza.make_pizza(17,'mushrooms', 'green peppers', 'extra cheese')
 
 from pizza import make_pizza
 make_pizza(16, 'pepperoni')
+
+from pizza import make_pizza as mp
+mp(16, 'pepperoni')
