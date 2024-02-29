2f2e01ce4f8017df59a870d610226312c1877ab0
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-02-29 18:21:23 +0900

    백준예제

--- practice/practice1.py
+++ practice/practice1.py
@@ -7,11 +7,19 @@
 # if __name__=="__main__":
 #     gugudan()
 
+#백준예제 윤달
+# year= int(input())
+# if year<=4000 and year>=1:
+#     if year%4==0 and (year%100!=0 or year%400==0):
+#         print("1")
+#     else:
+#         print("0")
+        
 
-year= int(input())
-year<=4000 and year>=1
-if year%4==0 and (year%100!=0 or year%400==0):
-    print("1")
-else:
-    print("0")
-    
+#백준예제-미완10950
+T=int(input())
+a=int(input())
+b=int(input())
+for i in range(T):
+    print(a+b)
+
