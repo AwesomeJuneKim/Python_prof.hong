86e9735ba7b4ab01106c56039dccb37fc8a8c139
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-02-29 17:59:14 +0900

    함수와 파라미터도 같이 전달

--- chapter08/forward_function.py
+++ chapter08/forward_function.py
@@ -11,3 +11,16 @@ def print_work():
     print('coding')
     
 ten_times(print_work)
+
+def add(x,y):
+    return x+y
+def minus(x,y):
+    return x-y
+
+def apply_operation(operation,x,y):
+    return operation(x,y)
+
+result = apply_operation(add,3,4)
+print(result)
+result2= apply_operation(minus,4,3)
+print(result2)
