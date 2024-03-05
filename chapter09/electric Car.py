a5d76b7a3da37bcd55838348f1e37658e8071860
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-03-05 14:59:30 +0900

    슈퍼클래스 연결

--- chapter09/electric Car.py
+++ chapter09/electric Car.py
@@ -1,22 +1,24 @@
-class Car:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-    def get_descriptive_name(self):
-        long_name=f"{self.year} {self.make} {self.model}"
-        return long_name.title()
-    def read_odometer(self):
-        print(f"This car has {self.odometer_reading} miles on it.")
-    def update_odometer(self, mileage):
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
-    def increment_odometer(self, miles):
-        self.odometer_reading += miles
-#2+배터리 클래스를 하나 더 만들어서 전기차에 연결시킨다.
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
+#     def get_descriptive_name(self):
+#         long_name=f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} miles on it.")
+#     def update_odometer(self, mileage):
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("You can't roll back an odometer!")
+#     def increment_odometer(self, miles):
+#         self.odometer_reading += miles
+# #2+배터리 클래스를 하나 더 만들어서 전기차에 연결시킨다.
+from car import Car
+#이것만 적어도 자동으로 연결되기 때문에 ElectricCar의 슈퍼클래스가 연결되어 에러가 뜨지 않는다.
 class Battery:
     def __init__(self, battery_size=40):
         self.battery_size = battery_size
