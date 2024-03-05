24cfd711cac529d382cec7d0de7b0abeac163bb6
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-03-05 14:51:57 +0900

    임포트 클래스를 간소화

--- chapter09/my_cars.py
+++ chapter09/my_cars.py
@@ -1,6 +1,6 @@
-import car
+import car as c
 
-my_mustang=car.Car("Ford", "Mustang", "2019")
+my_mustang=c.Car("Ford", "Mustang", "2019")
 print(my_mustang.get_descriptive_name())
-my_leaf=car.ElectricCar("Nissan", "Leaf", "2023")
+my_leaf=c.ElectricCar("Nissan", "Leaf", "2023")
 print(my_leaf.get_descriptive_name())
