dd254fd00eb09a4aa53fca63541588515a60c05b
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-03-05 14:48:47 +0900

    여러 클래스 임포트

---
+++ chapter09/my_cars.py
@@ -0,0 +1,6 @@
+from car import Car,ElectricCar
+
+my_mustang=ElectricCar("Ford", "Mustang", "2019")
+print(my_mustang.get_descriptive_name())
+my_leaf=ElectricCar("Nissan", "Leaf", "2023")
+print(my_leaf.get_descriptive_name())
